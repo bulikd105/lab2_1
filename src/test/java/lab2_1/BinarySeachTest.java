d92119902e8d3b009cd17a7741efd8f092921f63
bulikd105 <bulikd105@gmail.com>
2016-04-14 17:34:19 +0200

    Dodanie drugiego testu

--- src/test/java/lab2_1/BinarySeachTest.java
+++ src/test/java/lab2_1/BinarySeachTest.java
@@ -12,11 +12,20 @@ public class BinarySeachTest
 {
 
 	@Test
-	public void seqLength1_shouldFoundIndex() 
+	public void seqLengthOne_shouldFoundIndex() 
 	{
 		int[] seq = {1};
 		SearchResult sr = BinarySearch.search(1,seq);
 		assertThat(sr.isFound(), is(true));
+	}
+	
+	@Test
+	public void seqLengthLongerThanOne_shouldFoundIndexOne() 
+	{
+		int[] seq = {-3,5,6,7};
+		SearchResult sr = BinarySearch.search(-3,seq);
+		assertThat(sr.isFound(), is(true));
+		assertThat(sr.getPosition(),is(1));
 	}
 
 }
