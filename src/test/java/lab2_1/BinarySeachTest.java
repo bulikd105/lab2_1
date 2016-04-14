737aa7e287f8cceff76c3d34f9e88e53d1b3b4bc
bulikd105 <bulikd105@gmail.com>
2016-04-14 18:08:07 +0200

    Dodalem ostatnie dwa testy, oraz komentarze

--- src/test/java/lab2_1/BinarySeachTest.java
+++ src/test/java/lab2_1/BinarySeachTest.java
@@ -21,7 +21,7 @@ public class BinarySeachTest
 	
 	//Test - Dlugosc = 1, gdy element nie jest w sekwencji
 	@Test
-	public void lenOne_notFoundIndexOne()
+	public void lenOne_notFound()
 	{
 		int[] seq = {1};
 		SearchResult sr = BinarySearch.search(2, seq);
@@ -48,4 +48,22 @@ public class BinarySeachTest
 		assertThat(sr.getPosition(), is((seq.length+1)/2));
 	}
 	
+	//Test - Dlugosc > 1, gdy jest ostatnim elementem w sekwencji
+	@Test
+	public void lenLongerThanOne_foundIndexLast()
+	{
+		int[] seq = {-3,5,6,7};
+		SearchResult sr = BinarySearch.search(7, seq);
+		assertThat(sr.isFound(), is(true));
+		assertThat(sr.getPosition(), is((seq.length)));
+	}
+	
+	//Test - Dlugosc > 1, gdy elementu nie ma w sekwencji
+	@Test
+	public void lenLongerThanOne_notFound()
+	{
+		int[] seq = {-3,4,5,6,7};
+		SearchResult sr = BinarySearch.search(10, seq);
+		assertThat(sr.isFound(), is(false));
+	}
 }
